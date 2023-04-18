dc37e9e18edb955afb6d8b86de129cb205666e27
Muhammad Yasir Ghaffar <122939183+M-YasirGhaffar@users.noreply.github.com>
2023-04-18 20:52:49 +0500

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.random();
+randomNumber1 = 6*randomNumber1 + 1;
+randomNumber1 = Math.floor(randomNumber1);
+
+var randomNumber2 = Math.random();
+randomNumber2 = 6*randomNumber2 + 1;
+randomNumber2 = Math.floor(randomNumber2);
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+
+else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
